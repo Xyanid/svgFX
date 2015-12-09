d2b52c70bf078531bef1eaca837bba76e352dbd0
Xyanid <xyanid@web.de>
2015-12-09 22:41:38 +0100

    - adjusted project so it can be released on maven central

--- src/main/java/de/saxsys/svgfx/core/SVGParser.java
+++ src/main/java/de/saxsys/svgfx/core/SVGParser.java
@@ -1,3 +1,22 @@
+/*
+ *
+ * ******************************************************************************
+ *  * Copyright 2015 - 2015 Xyanid
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *   http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *  *****************************************************************************
+ */
+
 package de.saxsys.svgfx.core;
 
 import de.saxsys.svgfx.core.definitions.Enumerations;
@@ -21,7 +40,6 @@ public class SVGParser extends SAXParser<javafx.scene.Group, SVGDataProvider, SV
     /**
      * Creates a new instance of the parser and uses the given elementCreator.
      *
-     * @throws NoSuchMethodException thrown by the {@link SVGElementCreator} if an element does not have the required constructor
      */
     public SVGParser() {
 
