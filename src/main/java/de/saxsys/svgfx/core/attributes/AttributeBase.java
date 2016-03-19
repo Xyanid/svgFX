008c49688182d65df135dc3ee7ed1cafcecd15e7
Xyanid <xyanid@web.de>
2016-03-19 16:45:00 +0100

    - added attribute base

---
+++ src/main/java/de/saxsys/svgfx/core/attributes/AttributeBase.java
@@ -0,0 +1,54 @@
+package de.saxsys.svgfx.core.attributes;
+
+import de.saxsys.svgfx.content.ContentTypeBase;
+
+/**
+ * @author Xyanid on 16.03.2016.
+ */
+public abstract class AttributeBase<TSVGContentType extends ContentTypeBase> {
+
+    // region Fields
+
+    /**
+     * Contains the name of the attribute
+     */
+    private final String name;
+    /**
+     * This is the actual content of the attribute.
+     */
+    private final TSVGContentType content;
+
+    // region
+
+    // region Constructor
+
+    public AttributeBase(final String name, final TSVGContentType content) {
+        this.name = name;
+        this.content = content;
+    }
+
+    // endregion
+
+
+    // region Getter
+
+    /**
+     * Returns the {@link #name}.
+     *
+     * @return the {@link #name}.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the {@link #content}.
+     *
+     * @return the {@link #content}.
+     */
+    public TSVGContentType getContent() {
+        return content;
+    }
+
+    // endregion
+}
